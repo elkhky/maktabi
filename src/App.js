ffe57a0a89d9fb72f6541a67f6ea8019c32ce652
elkhky <marwanelkhky@gmail.com>
2026-06-30 17:20:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const DAYS_AR = ["أحد","اثنين","ثلاثاء","أربعاء","خميس",
 
 
 // ========== CLOUDINARY ==========
-const CLOUD_NAME = "dgplus11j";
+const CLOUD_NAME = "dgplus1lj";
 const UPLOAD_PRESET = "maktabi";
 
 async function uploadToCloudinary(file) {
