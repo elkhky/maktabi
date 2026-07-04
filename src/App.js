44a32bc4a284a66f23620f589b69892af26447f5
elkhky <marwanelkhky@gmail.com>
2026-07-04 19:51:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ async function uploadToCloudinary(file) {
   formData.append("file", file);
   formData.append("upload_preset", UPLOAD_PRESET);
   formData.append("resource_type", "auto");
-"https://api.cloudinary.com/v1_1/" + CLOUD_NAME + "/auto/upload"
+const res = await fetch("https://api.cloudinary.com/v1_1/" + CLOUD_NAME + "/auto/upload", {
   method: "POST", body: formData
   });
   const data = await res.json();
